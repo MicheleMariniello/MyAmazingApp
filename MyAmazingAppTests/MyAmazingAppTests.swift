3a808c76f1c3d8f2d1920b1121ad2db101e930d8
Michele Mariniello <michele-mariniello97@libero.it>
2024-11-04 15:19:04 +0100

    Initial Commit

---
+++ MyAmazingAppTests/MyAmazingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyAmazingAppTests.swift
+//  MyAmazingAppTests
+//
+//  Created by Michele Mariniello on 04/11/24.
+//
+
+import Testing
+@testable import MyAmazingApp
+
+struct MyAmazingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
